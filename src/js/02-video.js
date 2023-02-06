b27cb78fe7df245d6998fd2a54bcf754420496db
Iurii Bardych <iurii_bardych@hotmail.com>
2023-02-07 00:21:41 +0100

    added hw-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  const seconds = data.seconds;
+  console.log(seconds);
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+const throttledOnPlay = throttle(onPlay, 1000);
+
+player.on('timeupdate', throttledOnPlay);
+
+const seconds = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(Number.parseFloat(seconds) || 0);
